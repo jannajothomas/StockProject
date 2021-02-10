6486ac61940d53fb2cd836d0a4fe50d10ac46371
jannajothomas <jannajothomas@gmail.com>
2021-02-10 10:32:37 -0500

    Add comments and tests for Person

--- src/datamodels/Person.java
+++ src/datamodels/Person.java
@@ -2,9 +2,8 @@ package datamodels;
 
 import java.util.Calendar;
 import java.util.Objects;
-
 import interfaces.IPerson;
-//import utilities.DateFunctions;
+import utilities.DateFunctions;
 
 public class Person implements IPerson{
 	String name;
@@ -12,11 +11,21 @@ public class Person implements IPerson{
 	Calendar dateOfBirth;
 	Long id;
 	
-	//TODO: Implement this
+	/**
+	 * no-arg constuctor
+	 */
 	public Person() {
 		
 	}
 
+    /**
+     * This constructor takes the name, address, dateOfBirth, and id of a Person object
+     *
+     * @param name The name of the Investor object
+     * @param address The address of the Investor object
+     * @param dateOfBirth The dateOfBirth of the Investor object
+     * @param id The id of the Investor object
+     */
 	public Person(String name, String address, Calendar dateOfBirth, long id) {
 		this.name=name;
 		this.address=address;
@@ -24,34 +33,62 @@ public class Person implements IPerson{
 		this.id=id;
 	}
 	
+    /**
+     * Returns the name of the Person object
+     */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * sets the value of the name of the Person object
+	 * @param value
+	 */
 	public void setName(String value) {
 		name=value;
 	}
 	
+	/**
+     * Returns the address of the Person object
+     */
 	public String getAddress() {
 		return address;
 	}
 	
+	/**
+	 * sets the value of the address of the Person object
+	 * @param value
+	 */
 	public void setAddress(String value) {
 		address = value;
 	}
 	
+	/**
+     * Returns the dateOfBirth of the Person object
+     */
 	public Calendar getDateOfBirth() {
 		return dateOfBirth;
 	}
 	
+	/**
+	 * sets the value of the dateOfBirth of the Person object
+	 * @param value
+	 */
 	public void setDateOfBirth(Calendar value) {
 		dateOfBirth=value;
 	}
 	
+	/**
+     * Returns the id of the Person object
+     */
 	public long getId() {
 		return id;
 	}
 	
+	/**
+	 * sets the value of the id of the Person object
+	 * @param value
+	 */
 	public void setId(long value) {
 		this.id = value;
 	}
@@ -76,20 +113,45 @@ public class Person implements IPerson{
     
     @Override
 	public boolean equals(Object obj) {
-		return false;
-		//TODO  Implement this
+    	//The item you are comparing to is the exact object
+        if (this == obj) {
+            return true;
+        }
+        //The object is null.  This object is initialize and can't be null
+        if (obj == null) {
+            return false;
+        }
+        //If the objects are not the same class, they cannot be equal
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        final Investor other = (Investor) obj;
+        if(!Objects.equals(this.name, other.name)) {
+        	return false;
+        }
+        if(!Objects.equals(this.address, other.address)) {
+        	return false;
+        }
+        if(!Objects.equals(this.dateOfBirth, other.dateOfBirth)) {
+        	return false;
+        }
+        if(!Objects.equals(this.id, other.id)) {
+        	return false;
+        }
+        return true;
 	}
 	
     /**
-     * Returns a String representation of the StockQuote object in JSON format
+     * Returns a String representation of the Person object 
      */
-   // @Override
-   /* public String toString() {
+    @Override
+   public String toString() {
         return "Person: \n"
                 + "Name: " + this.name + "\n"
                 + "Address: " + this.address + "\n"
                 + "Date Of Birth: " + DateFunctions.dateToString(this.dateOfBirth) + "\n"
                 + "Id: " + this.id;
-    }*/
+    }
 	
 }
